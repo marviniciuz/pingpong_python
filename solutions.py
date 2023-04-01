681718c61ab5d3292d37febc4b5e36b2e211866d
Marcus Vinicius <eumarcusviniciusp@gmail.com>
2023-04-01 16:50:12 -0300

    finalizando paddings

--- solutions.py
+++ solutions.py
@@ -3,7 +3,7 @@ pygame.init()
 
 WIDTH, HEIGHT = 700,500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Pong")
+pygame.display.set_caption("PingPongPython")
 
 FPS = 60
 
@@ -23,10 +23,14 @@ class Paddle:
         self.height = height
     
     def draw(self,win):
-        pygame.draw.rectangle(win. self.COLOR, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
 
-def draw(win):
-    win.fill(WHITE)
+def draw(win,paddles):
+    win.fill(BLACK)
+
+    for paddle in paddles:
+        paddle.draw(win)
+
     pygame.display.update()
 
 
@@ -34,11 +38,12 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
-    left_paddle = Paddle(10,HEIGHT//2 - PADDLE_HEIGHT//2)
+    left_paddle = Paddle(10,HEIGHT//2 - PADDLE_HEIGHT//2,PADDLE_WIDHT,PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDHT,HEIGHT//2 - PADDLE_HEIGHT//2,PADDLE_WIDHT,PADDLE_HEIGHT)
 
     while run:
         clock.tick(FPS)
-        draw(WIN)
+        draw(WIN, [left_paddle,right_paddle])
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
