e588de0e5c3051e5a39aa4e1961ed44b6284f668
Marcus Vinicius <eumarcusviniciusp@gmail.com>
2023-04-07 16:24:43 -0300

    criando bola

--- solutions.py
+++ solutions.py
@@ -32,6 +32,26 @@ class Paddle:
         else:
             self.y += self.VEL
 
+
+class Ball:
+    MAX_VEL = 5
+    COLOR = WHITE
+
+    def __init__(self,x,y,radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.x_vel = MAX_VEL
+        self.y_vel = 0
+    
+    def draw(self,win):
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y),self.radius)
+
+    def move(self):
+        self.x += Self.x_vel
+        self.y += Self.y_vel
+
+
 def draw(win,paddles):
     win.fill(BLACK)
 
