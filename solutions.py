ca26ded810dc1a027a4946fb2240039574731637
Marcus Vinicius <eumarcusviniciusp@gmail.com>
2023-04-08 23:38:57 -0300

    criando a parte de mover a bola

--- solutions.py
+++ solutions.py
@@ -11,6 +11,7 @@ WHITE = (255,255,255)
 BLACK = (0,0,0)
 
 PADDLE_WIDHT,PADDLE_HEIGHT = 20, 100
+BALL_RADIUS = 7
 
 
 class Paddle:
@@ -37,22 +38,23 @@ class Ball:
     MAX_VEL = 5
     COLOR = WHITE
 
+
     def __init__(self,x,y,radius):
         self.x = x
         self.y = y
         self.radius = radius
-        self.x_vel = MAX_VEL
+        self.x_vel = self.MAX_VEL
         self.y_vel = 0
     
     def draw(self,win):
         pygame.draw.circle(win, self.COLOR, (self.x, self.y),self.radius)
 
     def move(self):
-        self.x += Self.x_vel
-        self.y += Self.y_vel
+        self.x += self.x_vel
+        self.y += self.y_vel
 
 
-def draw(win,paddles):
+def draw(win,paddles,ball):
     win.fill(BLACK)
 
     for paddle in paddles:
@@ -63,7 +65,7 @@ def draw(win,paddles):
                 continue
             pygame.draw.rect(win,WHITE,(WIDTH//2 - 5,i,10,HEIGHT//20))
 
-
+    ball.draw(win)
     pygame.display.update()
 
 def handle_paddle_movement(keys,left_paddle, right_paddle):
@@ -77,6 +79,7 @@ def handle_paddle_movement(keys,left_paddle, right_paddle):
     if keys[pygame.K_DOWN]and right_paddle.y + right_paddle.VEL + right_paddle.height <= HEIGHT:
         right_paddle.move(up=False)
 
+
 def main():
     run = True
     clock = pygame.time.Clock()
@@ -84,9 +87,11 @@ def main():
     left_paddle = Paddle(10,HEIGHT//2 - PADDLE_HEIGHT//2,PADDLE_WIDHT,PADDLE_HEIGHT)
     right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDHT,HEIGHT//2 - PADDLE_HEIGHT//2,PADDLE_WIDHT,PADDLE_HEIGHT)
 
+    ball = Ball(WIDTH//2,HEIGHT//2,BALL_RADIUS)
+
     while run:
         clock.tick(FPS)
-        draw(WIN, [left_paddle,right_paddle])
+        draw(WIN, [left_paddle,right_paddle],ball)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -95,6 +100,8 @@ def main():
 
         keys = pygame.key.get_pressed()
         handle_paddle_movement(keys, left_paddle,right_paddle)
+
+        ball.move()
 
     pygame.quit()
 
