4d47521150da4b8752d93ab9788f43a8d6281689
Marcus Vinicius <eumarcusviniciusp@gmail.com>
2023-04-09 20:40:01 -0300

    ampliando campo de colisão da bola

--- solutions.py
+++ solutions.py
@@ -34,6 +34,7 @@ class Paddle:
             self.y += self.VEL
 
 
+#criando a bola 
 class Ball:
     MAX_VEL = 5
     COLOR = WHITE
@@ -48,12 +49,12 @@ class Ball:
     
     def draw(self,win):
         pygame.draw.circle(win, self.COLOR, (self.x, self.y),self.radius)
-
+#criando eixos para mover a bola
     def move(self):
         self.x += self.x_vel
         self.y += self.y_vel
 
-
+#criando a barreiras
 def draw(win,paddles,ball):
     win.fill(BLACK)
 
@@ -68,6 +69,35 @@ def draw(win,paddles,ball):
     ball.draw(win)
     pygame.display.update()
 
+def hand_collision(ball,left_paddle, right_paddle):
+    if ball.y + ball.radius >= HEIGHT:
+        ball.y_vel *= -1
+    elif ball.y - ball.radius <= 0:
+        ball.y_vel *= -1
+    
+    if ball.x_vel < 0:
+        if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
+            if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
+                ball.x_vel *= -1
+
+                middle_y = left_paddle.y + left_paddle.height/2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (left_paddle.height/2)/ball.MAX_VEL
+                y_vel = difference_in_y / reduction_factor
+                ball.y_vel = y_vel
+    else:
+        if ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
+            if ball.x + ball.radius >= right_paddle.x:
+                ball.x_vel *= -1
+    
+                middle_y = right_paddle.y + right_paddle.height/2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (right_paddle.height/2)/ball.MAX_VEL
+                y_vel = difference_in_y / reduction_factor
+                ball.y_vel = y_vel
+
+
+#movimentando as barreiras
 def handle_paddle_movement(keys,left_paddle, right_paddle):
     if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:
         left_paddle.move(up=True)
@@ -102,6 +132,7 @@ def main():
         handle_paddle_movement(keys, left_paddle,right_paddle)
 
         ball.move()
+        hand_collision(ball,left_paddle,right_paddle)
 
     pygame.quit()
 
